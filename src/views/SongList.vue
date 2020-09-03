562fa9b78fc6810bf3a023c7987a70b9d4598957
mingyuLi97 <1154479896@qq.com>
2020-09-03 21:12:52 +0800

    歌单广场界面开发

--- src/views/SongList.vue
+++ src/views/SongList.vue
@@ -1,36 +1,59 @@
 <template>
   <div>
-    <back-bar>
-      <span>歌单广场</span>
-    </back-bar>
-    <van-tabs @change='changeCategory'>
+    <van-tabs @change='changeCategory' :before-change='beforeChangeCategory'>
       <van-tab
         v-for="(catName, index) in categoryNameArr"
         :key="index"
         :title="catName">
-        {{ catName }}
+        
+        <div class="cover-list" @scroll="handleScroll" :style="{height:coverListHeight}">
+          <cover
+            v-for="(item, index) in curSonglist"
+            :key="index"
+            :count='item.playCount'
+            :url='item.coverImgUrl'
+            :name='item.name'
+            class="cover-item"
+          />
+          <div class="list-tip">{{listTip}}</div>
+        </div>
+        
       </van-tab>
     </van-tabs>
   </div>
 </template>
 
 <script>
-import BackBar from '@/components/common/BackBar';
+import Cover from '@/components/common/Cover';
+import { mapGetters } from 'vuex';
 export default {
   components:{
-    BackBar
+    Cover
   },
   mounted(){
     this._getHotCatList();
   },
+  computed:{
+    ...mapGetters(['musicListLen']),
+    coverListHeight(){
+      return this.musicListLen ? '139.467vw' : '152.8vw';
+    },
+    listTip(){
+      return this.finished ? '没有更多了 ' : '加载中...';
+    }
+  },
   data() { 
     return {
       categoryNameArr: [],
+      curCategoryIndex: 0,
+      cacheSonglist: [],  // 缓存所有加载过的页面数据
       limit: 15,
       offset: 0,
       category: '全部',
       order: 'hot',
-      curSonglist: []
+      curSonglist: [],  // 页面渲染依赖的数组
+      loading: false,
+      finished: true,
     };
   },
   methods:{
@@ -40,7 +63,6 @@ export default {
         console.log('!!!Error: _getHotCatList', res.code);
         return;
       }
-      console.log(res);
       this.categoryNameArr = res.tags.map(item => item.name);
       
       if(this.categoryNameArr.length){
@@ -49,6 +71,7 @@ export default {
       }
     },
     async _getPlaylistByCat(){
+      console.log('call _getPlaylistByCat');
       const res = await this.$api.getPlaylistByCat(
         this.category,
         this.limit,
@@ -59,18 +82,63 @@ export default {
         console.log('!!!Error: _getPlaylistByCat', res.code);
         return;
       }
-      console.log(res);
+      this.finished = !res.more;
+
+      this.loading = false;
+      this.curSonglist = this.curSonglist.concat(res.playlists);
+    },
+    beforeChangeCategory(name){
+      // 跳转的是当前页面直接返回
+      if(name === this.curCategoryIndex) return false;
+      // 缓存当前页面数据
+      this.cacheSonglist[this.curCategoryIndex] = this.curSonglist;
+      return true;
     },
     changeCategory(name, title){
       // name 相当于索引 0 1 2
-      console.log(name, title);
+      this.curCategoryIndex = name;
       this.category = title;
-      this.offset = 0;
-      this._getPlaylistByCat();
+      // 先从缓存中获取数据
+      this.curSonglist = this.cacheSonglist[this.curCategoryIndex] || [];
+      this.offset = this.curSonglist.length;
+      !this.curSonglist.length && this._getPlaylistByCat();
+    },
+    handleScroll(e){
+      if(this.loading || this.finished) return;
+    
+      // 滚动条距离顶部的距离
+      let scrollTop = e.target.scrollTop;
+      // 整个窗体的高度
+      let clientHeight = e.target.clientHeight;
+      // 整个滚动条的高度
+      let scrollHeight = e.target.scrollHeight;
+
+      if(scrollTop + clientHeight >= scrollHeight){
+        this.loading = true;
+        this.offset++;
+        this._getPlaylistByCat();
+      }
     }
   }
 };
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
+
+.cover-list {
+  overflow: scroll;
+  padding: 0 2vw;
+  .cover-item{
+    display: inline-block;
+    width: 29.333vw;
+    margin: 2.667vw 1.333vw;
+    vertical-align: top;  // 行内元素存在基线 会居中对齐 当文字是一行的时候会导致塌陷
+  }
+  .list-tip{
+    line-height: 13.333vw;
+    text-align: center;
+    font-size: 3.733vw;
+    color: #999999;
+  }
+}
 </style>
